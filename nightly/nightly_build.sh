a78fd1ce3c60d2d4c7d90e1bfc9b8bd5fc3ca20c
Eric Stokes <fernferret@gmail.com>
2011-01-21 04:41:48 -0500

    Now script pulls from git

--- nightly/nightly_build.sh
+++ nightly/nightly_build.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 # Android Nightly Build Script
+
+# Params
+BUILD_DIR=~/MTMNightly/
+REPO_EXISTS_FOLDER=.git/
+GIT_REPO=git://github.com/Mobile-Trail-Mapping/Android.git
+
+# Pull the latest changes from the Android master branch
+if [ -d "${BUILD_DIR}${REPO_EXISTS_FOLDER}" ]; then
+  echo "Git repo exists, updating"
+  cd $BUILD_DIR
+  git status
+  git pull
+else
+  echo "No Repo found, cloning..."
+  git clone $GIT_REPO "${BUILD_DIR}${REPO_DIR}"
+fi
