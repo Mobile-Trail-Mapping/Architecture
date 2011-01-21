09047a55217592d72bd2b3039addddadaeb383d3
Eric Stokes <fernferret@gmail.com>
2011-01-21 05:26:48 -0500

    Builds work from one line

--- nightly/nightly_build.sh
+++ nightly/nightly_build.sh
@@ -4,6 +4,7 @@
 # Params
 BUILD_DIR=~/MTMNightly
 ANDROID_PROJECT_DIR=/Android
+ANDROID_DIR="${BUILD_DIR}${ANDROID_PROJECT_DIR}"
 REPO_EXISTS_FOLDER=/.git
 
 KEYSTORE_DIR=.
@@ -12,25 +13,42 @@ SECURE_PROP_DIR=.
 GIT_REPO=git://github.com/Mobile-Trail-Mapping/Android.git
 GIT_BRANCH=advanced-build
 
+PATH_TO_ME=$PWD
 # Pull the latest changes from the Android master branch
 if [ -d "${BUILD_DIR}${REPO_EXISTS_FOLDER}" ]; then
   echo "Git repo exists, updating"
   cd $BUILD_DIR
   git status
   git pull
+  cd $PATH_TO_ME
+  echo "Adding secure.properties"
+  cp "${SECURE_PROP_DIR}/secure.properties" "${ANDROID_DIR}/secure.properties"
+  
+  echo "Adding brousalis.keystore"
+  cp "${KEYSTORE_DIR}/brousalis.keystore" "${ANDROID_DIR}/brousalis.keystore"
+  
+  echo "Adding secret XML file"
+  cp "${KEYSTORE_DIR}/secret.xml" "${ANDROID_DIR}/res/values/secret.xml"
 else
   echo "No Repo found, cloning..."
   git clone --branch $GIT_BRANCH $GIT_REPO "${BUILD_DIR}"
   
+  cd $PATH_TO_ME
+  
   echo "Adding secure.properties"
-  cp "${SECURE_PROP_DIR}/secure.properties" $BUILD_DIR
+  cp "${SECURE_PROP_DIR}/secure.properties" "${ANDROID_DIR}/secure.properties"
   
   echo "Adding brousalis.keystore"
-  cp "${KEYSTORE_DIR}/brousalis.keystore" $BUILD_DIR
+  cp "${KEYSTORE_DIR}/brousalis.keystore" "${ANDROID_DIR}/brousalis.keystore"
+  
+  echo "Adding secret XML file"
+  cp "${KEYSTORE_DIR}/secret.xml" "${ANDROID_DIR}/res/values/secret.xml"
   
   cd $BUILD_DIR
 fi
 
 echo "Building Android"
-
-ls "${PWD}${ANDROID_PROJECT_DIR}"
+cd $ANDROID_DIR
+echo $ANDROID_DIR
+android update project --path .
+ant release
