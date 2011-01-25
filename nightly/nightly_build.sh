118f4c7dca64a83f7680544473e5d797caf39a75
Eric Stokes <fernferret@gmail.com>
2011-01-25 12:26:45 -0500

    Add Multiple Git Repos

--- nightly/nightly_build.sh
+++ nightly/nightly_build.sh
@@ -7,7 +7,6 @@ ANDROID_PROJECT_DIR=/Android
 ANDROID_DIR="${BUILD_DIR}${ANDROID_PROJECT_DIR}"
 REPO_EXISTS_FOLDER=/.git
 
-KEYSTORE_DIR=.
 SECURE_PROP_DIR=.
 
 UPLOAD_PASSWORD_FILE=password.txt
@@ -18,47 +17,45 @@ GIT_BRANCH=advanced-build
 read -r PASSWORD < "password.txt"
 
 PATH_TO_ME=$PWD
+git branch
 # Pull the latest changes from the Android master branch
 if [ -d "${BUILD_DIR}${REPO_EXISTS_FOLDER}" ]; then
-  echo "Git repo exists, updating"
-  cd $BUILD_DIR
-  git status
-  git pull
-  cd $PATH_TO_ME
-  echo "Adding secure.properties"
-  cp "${SECURE_PROP_DIR}/secure.properties" "${ANDROID_DIR}/secure.properties"
-  
-  echo "Adding brousalis.keystore"
-  cp "${KEYSTORE_DIR}/brousalis.keystore" "${ANDROID_DIR}/brousalis.keystore"
-  
-  echo "Adding secret XML file"
-  cp "${KEYSTORE_DIR}/secret.xml" "${ANDROID_DIR}/res/values/secret.xml"
-else
+  rm -rf "${BUILD_DIR}"
+  mkdir "${BUILD_DIR}"
+fi
   echo "No Repo found, cloning..."
   git clone --branch $GIT_BRANCH $GIT_REPO "${BUILD_DIR}"
-  
+  cd $BUILD_DIR
+#  read -r BRANCHES < temp-branches.txt
+  #BRANCHES=( "fish" "dog" "bear" )
+  echo ""
+  git branch -r | while read line; do
+      echo $line | tr "/" " "
+      parts=(`echo $line | tr "/" " "`)
+      echo ${parts[1]}
+      echo ""
+  done
+  echo ""
   cd $PATH_TO_ME
-  
   echo "Adding secure.properties"
   cp "${SECURE_PROP_DIR}/secure.properties" "${ANDROID_DIR}/secure.properties"
   
   echo "Adding brousalis.keystore"
-  cp "${KEYSTORE_DIR}/brousalis.keystore" "${ANDROID_DIR}/brousalis.keystore"
+  cp "${SECURE_PROP_DIR}/brousalis.keystore" "${ANDROID_DIR}/brousalis.keystore"
   
   echo "Adding secret XML file"
-  cp "${KEYSTORE_DIR}/secret.xml" "${ANDROID_DIR}/res/values/secret.xml"
+  cp "${SECURE_PROP_DIR}/secret.xml" "${ANDROID_DIR}/res/values/secret.xml"
   
   cd $BUILD_DIR
-fi
 
 echo "Building Android"
 cd $ANDROID_DIR
 echo $ANDROID_DIR
-android update project --path .
-ant release
+#android update project --path .
+#ant release
 ls "${ANDROID_DIR}"
 echo "Attempting to upload."
-scp "${ANDROID_DIR}/bin/ShowMap-release.apk" "fernferr@eric-stokes.com:public_html/mtm/nightly/builds/MTMBeta_Nightly.TEST.apk"
+#scp "${ANDROID_DIR}/bin/ShowMap-release.apk" "fernferr@eric-stokes.com:public_html/mtm/nightly/builds/MTMBeta_Nightly.TEST.apk"
 SUCCESS=$?
 if [ $SUCCESS ]; then
     echo "Upload COMPLETE."
